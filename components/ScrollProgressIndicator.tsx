9d13f8ee723510db0a067446f63aebd36a6dda75
Tajmirul <tasmirolislam@gmail.com>
2024-11-19 05:46:10 +0600

    progress bar update for mobile

--- components/ScrollProgressIndicator.tsx
+++ components/ScrollProgressIndicator.tsx
@@ -25,7 +25,7 @@ const ScrollProgressIndicator = () => {
     }, []);
 
     return (
-        <div className="fixed top-1/2 right-[2%] w-1.5 h-[100px] rounded-full bg-background-light overflow-hidden">
+        <div className="fixed top-[50svh] right-[2%] -translate-y-1/2 w-1.5 h-[100px] rounded-full bg-background-light overflow-hidden">
             <div
                 className="w-full bg-primary rounded-full h-full"
                 ref={scrollBarRef}
