1dc8da659e8dc1fca6784e29033b66a1c4a05a4f
Tajmirul <tasmirolislam@gmail.com>
2024-11-25 23:57:33 +0600

    project external links are close to the title

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,6 +1,23 @@
 import { notFound } from 'next/navigation';
 import ProjectDetails from './_components/ProjectDetails';
 import { PROJECTS } from '@/lib/data';
+import { Metadata } from 'next';
+
+export const generateMetadata = ({
+    params,
+}: {
+    params: { slug: string };
+}): Metadata => {
+    const { slug } = params;
+    const project = PROJECTS.find((project) => project.slug === slug);
+
+    return {
+        title: `${project?.title} - ${project?.techStack
+            .slice(0, 3)
+            .join(', ')}`,
+        description: project?.description,
+    };
+};
 
 const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
     const { slug } = await params;
