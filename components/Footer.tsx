f5c3d2b99a54609e3e32283e2729db5be22a9a5b
Tajmirul <tasmirolislam@gmail.com>
2025-04-08 16:48:53 +0600

    update: copyright message

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,20 @@
 import { GENERAL_INFO } from '@/lib/data';
-import React from 'react';
+import { GitFork, Star } from 'lucide-react';
 
-const Footer = () => {
+interface RepoStats {
+    stargazers_count: number;
+    forks_count: number;
+}
+
+const Footer = async () => {
+    const repoStats = await fetch('https://api.github.com/repos/tajmirul/portfolio-2.0', {
+        next: {
+            revalidate: 60 * 60, // 1 hour
+        },
+    })
+
+    const { stargazers_count, forks_count } = await repoStats.json() as RepoStats;
+    
     return (
         <footer className="text-center pb-5" id="contact">
             <div className="container">
@@ -12,9 +25,38 @@ const Footer = () => {
                 >
                     {GENERAL_INFO.email}
                 </a>
-                <p className="text-muted-foreground">
-                    Copyright © {new Date().getFullYear()} Tajmirul
-                </p>
+
+                <div className="">
+                    <a href='https://github.com/Tajmirul/portfolio-2.0' target='_blank' className="leading-none text-muted-foreground hover:underline hover:text-white">
+                        Design & built by Tajmirul Islam
+
+                        <div className="flex items-center justify-center gap-5 pt-1">
+                            <span className='flex items-center gap-2'>
+                                <Star size={18} /> {stargazers_count}
+                            </span>
+                            <span className='flex items-center gap-2'>
+                                <GitFork size={18} /> {forks_count}
+                            </span>
+                        </div>
+                    </a>
+
+                    {/* Note: If you are not Tajmirul, use this copyright message instead */}
+                    {/* <a href='https://github.com/Tajmirul/portfolio-2.0' className="leading-none text-muted-foreground hover:underline hover:text-white">
+                        Design & built by Tajmirul Islam <br />
+                        Revised by YOUR NAME
+
+                        <div className="flex items-center justify-center gap-5 pt-1">
+                            <span className='flex items-center gap-2'>
+                                <Star size={14} /> {stargazers_count}
+                            </span>
+                            <span className='flex items-center gap-2'>
+                                <GitFork size={14} /> {forks_count}
+                            </span>
+                        </div>
+                    </a> */}
+                </div>
+
+                
             </div>
         </footer>
     );
