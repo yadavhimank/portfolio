195a27e6ace1d7fbf2cea25bd1a43a48087721ce
Tajmirul <tasmirolislam@gmail.com>
2025-04-13 19:43:07 +0600

    update copyright url

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,14 +7,18 @@ interface RepoStats {
 }
 
 const Footer = async () => {
-    const repoStats = await fetch('https://api.github.com/repos/tajmirul/portfolio-2.0', {
-        next: {
-            revalidate: 60 * 60, // 1 hour
+    const repoStats = await fetch(
+        'https://api.github.com/repos/tajmirul/portfolio-2.0',
+        {
+            next: {
+                revalidate: 60 * 60, // 1 hour
+            },
         },
-    })
+    );
 
-    const { stargazers_count, forks_count } = await repoStats.json() as RepoStats;
-    
+    const { stargazers_count, forks_count } =
+        (await repoStats.json()) as RepoStats;
+
     return (
         <footer className="text-center pb-5" id="contact">
             <div className="container">
@@ -27,21 +31,24 @@ const Footer = async () => {
                 </a>
 
                 <div className="">
-                    <a href='https://github.com/Tajmirul/portfolio-2.0' target='_blank' className="leading-none text-muted-foreground hover:underline hover:text-white">
+                    <a
+                        href="https://github.com/Tajmirul/portfolio-2.0"
+                        target="_blank"
+                        className="leading-none text-muted-foreground hover:underline hover:text-white"
+                    >
                         Design & built by Tajmirul Islam
-
                         <div className="flex items-center justify-center gap-5 pt-1">
-                            <span className='flex items-center gap-2'>
+                            <span className="flex items-center gap-2">
                                 <Star size={18} /> {stargazers_count}
                             </span>
-                            <span className='flex items-center gap-2'>
+                            <span className="flex items-center gap-2">
                                 <GitFork size={18} /> {forks_count}
                             </span>
                         </div>
                     </a>
 
                     {/* Note: If you are not Tajmirul, use this copyright message instead */}
-                    {/* <a href='https://github.com/Tajmirul/portfolio-2.0' className="leading-none text-muted-foreground hover:underline hover:text-white">
+                    {/* <a href='https://www.me.toinfinite.dev/' className="leading-none text-muted-foreground hover:underline hover:text-white">
                         Design & built by Tajmirul Islam <br />
                         Revised by YOUR NAME
 
@@ -55,8 +62,6 @@ const Footer = async () => {
                         </div>
                     </a> */}
                 </div>
-
-                
             </div>
         </footer>
     );
