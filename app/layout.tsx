64eeb68dce68ba3c75c6cbbbead9c6c2297a274e
tajmirul <tasmirolislam@gmail.com>
2025-04-23 17:16:51 +0600

    add: hotjar integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import CustomCursor from '@/components/CustomCursor';
 import Preloader from '../components/Preloader';
 import StickyEmail from './_components/StickyEmail';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from 'next/script';
 
 const antonFont = Anton({
     weight: '400',
@@ -40,6 +41,16 @@ export default function RootLayout({
     return (
         <html lang="en">
             <GoogleAnalytics gaId="G-MHLY1LNGY5" />
+            <Script id="hotjar" strategy="afterInteractive">
+                {`(function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:6380611,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+            </Script>
             <body
                 className={`${antonFont.variable} ${robotoFlex.variable} antialiased`}
             >
