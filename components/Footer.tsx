10b58e584ee2d5e68154713e62ac200d08158f3b
himankyadav02@gmail.com <himankyadav02@gmail.com>
2025-05-15 23:34:56 +0530

    footer error  fixed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,24 +1,6 @@
 import { GENERAL_INFO } from '@/lib/data';
-import { GitFork, Star } from 'lucide-react';
-
-interface RepoStats {
-    stargazers_count: number;
-    forks_count: number;
-}
 
 const Footer = async () => {
-    const repoStats = await fetch(
-        'https://api.github.com/repos/tajmirul/portfolio-2.0',
-        {
-            next: {
-                revalidate: 60 * 60, // 1 hour
-            },
-        },
-    );
-
-    const { stargazers_count, forks_count } =
-        (await repoStats.json()) as RepoStats;
-
     return (
         <footer className="text-center pb-5" id="contact">
             <div className="container">
