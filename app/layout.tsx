9f6eaf2aa0226fb24a1e7dbf985c82b658618224
himankyadav02@gmail.com <himankyadav02@gmail.com>
2025-05-15 23:31:39 +0530

    type script error fixed in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,6 @@ import Navbar from '@/components/Navbar';
 import CustomCursor from '@/components/CustomCursor';
 import Preloader from '../components/Preloader';
 import StickyEmail from './_components/StickyEmail';
-import { GoogleAnalytics } from '@next/third-parties/google';
-import Script from 'next/script';
 
 const antonFont = Anton({
     weight: '400',
